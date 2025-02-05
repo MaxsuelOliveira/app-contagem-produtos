129f054851aa6bf6c77a89fbcc6d10240bd2c7b6
Maxsuel David <oliveiramaxsuellll@gmail.com>
2025-02-05 03:10:58 -0300

    remove arquivo de utilitário para exportação de planilhas

--- src/utils/exportSpreadSheets.js
+++
@@ -1,8 +0,0 @@
-import { useNavigation } from "@react-navigation/native";
-import * as FileSystem from "expo-file-system";
-import * as Sharing from "expo-sharing";
-import { Alert } from "react-native";
-import * as XLSX from "xlsx";
-
-import { Controller } from "../services/backend/controller";
-
