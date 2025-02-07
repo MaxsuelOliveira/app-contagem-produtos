d4602901e0e0995c165ccc62e90a4e81242d62c8
Maxsuel David <oliveiramaxsuellll@gmail.com>
2025-02-07 01:07:44 -0300

    adiciona tela de criação de conta com validações, formatação de inputs e melhorias de estilo

--- src/screens/CreateAccount/CreateAccount.js
+++ src/screens/CreateAccount/CreateAccount.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import uuid from "react-native-uuid";
 import { StatusBar } from "expo-status-bar";
-import { View, Text, TextInput, TouchableOpacity, Alert, ScrollView} from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  ScrollView,
+} from "react-native";
 
 // Icons
 import AntDesign from "@expo/vector-icons/AntDesign";
@@ -10,45 +17,176 @@ import AntDesign from "@expo/vector-icons/AntDesign";
 import { GlobalStyles, colors } from "../../styles/GlobalStyles";
 import { styles } from "./styles";
 import Checkbox from "expo-checkbox";
+import { FormattingInputs } from "../../utils/inputFormat";
 
 const CreateAccount = () => {
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [focusInName, setFocusInName] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [cnpj, setCnpj] = useState("");
+  const [nameFantasy, setNameFantasy] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [terms, setTerms] = useState(false);
 
-  function createInventory() {
-    if (name.length == 0 || name == "") {
-      Alert.alert("O inventário precisa de um nome !");
+  const [focusInCnpj, setFocusInCnpj] = useState(false);
+  const [focusInName, setFocusInName] = useState(true);
+  const [focusInEmail, setFocusInEmail] = useState(false);
+  const [focusInPhone, setFocusInPhone] = useState(false);
+  const [focusInTerms, setFocusInTerms] = useState(false);
+
+  function create() {
+    if (cnpj.length == 0 || cnpj == "" || cnpj.length < 18) {
+      Alert.alert("O cnpj da empresa é obrigatório !");
+      setFocusInCnpj(true);
+      return;
+    }
+
+    if (nameFantasy.length == 0 || nameFantasy == "") {
+      Alert.alert("O nome fantasia é obrigatório !");
       setFocusInName(true);
       return;
     }
 
-    const inventory = {
-      uuid: uuid.v4(),
-      name: name,
-      describe: description,
-      status: "progress", // progress | done | unknown
-      date_create: new Date(),
-      date_end: new Date(),
-      products: [],
-      compare_in_spreadsheet: false,
-      compare_price: false,
+    if (email.length == 0 || email == "") {
+      Alert.alert("O email é obrigatório !");
+      setFocusInEmail(true);
+      return;
+    }
+
+    if (phone.length == 0 || phone == "" || phone.length < 15) {
+      Alert.alert("O telefone é obrigatório !");
+      setFocusInPhone(true);
+      return;
+    }
+
+    if (!terms) {
+      Alert.alert(
+        "Você precisa aceitar os termos de uso e política de privacidade."
+      );
+      setFocusInTerms(true);
+      return;
+    }
+
+    const bussines = {
+      cnpj: cnpj,
+      nameFantasy: nameFantasy,
+      email: email,
+      phone: phone,
+      password: "estoqueFácil123",
     };
+
+    console.log("Bussines: ", bussines);
   }
 
   return (
-    <View style={styles.settingsContainer}>
+    <View style={{ ...GlobalStyles.container, backgroundColor: "#ffffff" }}>
       <StatusBar style="auto" backgroundColor="#ffffff" />
 
-      <View style={GlobalStyles.cardHeader}>
-        <Text style={styles.cardTilte}>
-          Criar Conta
-        </Text>
-      </View>
+      <View style={styles.card}>
+        <View style={styles.cardHeader}>
+          <Text style={styles.cardTitle}>Criar Conta</Text>
+          <Text style={GlobalStyles.label}>
+            Crie uma nova conta para começar a usar o aplicativo. Teste grátis
+            por 7 dias.
+          </Text>
+        </View>
 
-      <ScrollView>
-        <Text style={styles.cardTitle}>Criar Conta</Text>
-      </ScrollView>
+        <View
+          style={{
+            ...GlobalStyles.cardBody,
+            alignItems: "center",
+            justifyContent: "center",
+            flex: 1,
+          }}
+        >
+          <View style={styles.marginBottom}>
+            <Text style={GlobalStyles.label}>CNPJ *</Text>
+            <TextInput
+              style={styles.input}
+              value={cnpj}
+              onChangeText={(text) => {
+                text = FormattingInputs.formatCNPJ(text);
+                setCnpj(text);
+              }}
+              minLength={18}
+              maxLength={18}
+              keyboardType="numeric"
+              autoFocus={focusInCnpj}
+            />
+          </View>
+
+          <View style={styles.marginBottom}>
+            <Text style={GlobalStyles.label}>Nome Fantasia*</Text>
+            <TextInput
+              style={styles.input}
+              value={nameFantasy}
+              onChangeText={(text) => setNameFantasy(text)}
+              keyboardType="default"
+              autoFocus={focusInName}
+            />
+          </View>
+
+          <View style={styles.marginBottom}>
+            <Text style={GlobalStyles.label}>Email *</Text>
+            <TextInput
+              style={styles.input}
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              keyboardType="email-address"
+            />
+          </View>
+
+          <View style={styles.marginBottom}>
+            <Text style={GlobalStyles.label}>Telefone *</Text>
+            <TextInput
+              style={styles.input}
+              value={phone}
+              maxLength={15}
+              onChangeText={(text) => {
+                text = FormattingInputs.formatPhone(text);
+                setPhone(text);
+              }}
+              keyboardType="phone-pad"
+              autoFocus={focusInPhone}
+            />
+          </View>
+
+          <View style={{ ...styles.inputGroup, marginBottom: 20 }}>
+            <View style={{ width: "90%" }}>
+              <Text style={GlobalStyles.label}>
+                Termos de uso e política de privacidade *
+              </Text>
+              <Text style={GlobalStyles.label}>
+                Ao criar uma conta, você concorda com os nossos Termos de {""}
+                <Text style={{ ...GlobalStyles.link, borderBottomWidth: 0 }}>
+                  Política de Privacidade. {"\n\n"}
+                </Text>
+                Também você concorda sobre as leis de proteção de dados.
+              </Text>
+            </View>
+            <Checkbox
+              value={terms}
+              onValueChange={() => {
+                setTerms(!terms);
+              }}
+              autoFocus={focusInTerms}
+              color={colors.primary}
+              style={GlobalStyles.checkbox}
+            />
+          </View>
+
+          <TouchableOpacity
+            style={GlobalStyles.button}
+            onPress={() => create()}
+          >
+            <Text style={GlobalStyles.buttonText}>Criar Conta</Text>
+            <Text
+              style={{ ...GlobalStyles.small, color: "#ffffff", marginTop: 0 }}
+            >
+              Testar por 7 dias grátis
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/CreateAccount/styles.js
+++ src/screens/CreateAccount/styles.js
@@ -2,15 +2,39 @@ import { StyleSheet } from "react-native";
 import { GlobalStyles, colors } from "../../styles/GlobalStyles";
 
 export const styles = StyleSheet.create({
-  cardTitle: {
-    ...GlobalStyles.cardTitle,
-    marginTop: 0,
-    fontSize: 22,
+  card: {
+    flex: 1,
+    height: "100vh",
+    backgroundColor: colors.white,
+    padding: 20,
   },
 
-  textarea: {
-    ...GlobalStyles.input,
-    ...GlobalStyles.textArea,
-    paddingTop: 10,
+  cardHeader: {
+    marginBottom: 20,
   },
+
+  cardTitle: {
+    // ...GlobalStyles.cardTitle,
+    marginTop: 0,
+    fontSize: 22,
+    fontFamily : "Montserrat_Medium",
+    marginBottom: 5,
+  },
+
+  inputGroup: {
+    width: "100%",
+    flex: 1,
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+
+  marginBottom : {
+    marginBottom : 10
+  },
+
+  input : {
+    ...GlobalStyles.input,
+    minWidth: "100%"
+  }
+
 });
